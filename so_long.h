9cf0fc0ddaf7d9423fb8cef05a4bac545f1d4d81
Badreddine Chiki <bchiki@c3r1p7.1337.ma>
2025-03-19 03:32:02 +0000

    loooooooong

--- so_long.h
+++ so_long.h
@@ -6,7 +6,24 @@
 /*   By: bchiki <bchiki@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 01:08:25 by bchiki            #+#    #+#             */
-/*   Updated: 2025/03/19 01:08:28 by bchiki           ###   ########.fr       */
+/*   Updated: 2025/03/19 03:30:54 by bchiki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef SO_LONG_H
+#define SO_LONG_H
+
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <limits.h>
+#include "mlx/mlx.h"
+#include "not_your_printf/ft_printf.h"
+
+typedef struct s_game 
+{
+    void *mlx;
+    void *win;
+}               t_game;
+
+#endif
